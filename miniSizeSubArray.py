430e393deb9935b35af563d70823db53973edddf
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-10-12 12:21:18 +0300

    Create miniSizeSubArray.py

---
+++ miniSizeSubArray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        l=0
+        total=0
+        k = len(nums)+1
+        for i in range(len(nums)):
+            total += nums[i]
+            while total >=target:
+                k =  min((i-l)+1,k)
+                total -= nums[l]
+                l +=1
+        if k != len(nums)+1:
+            return k
+        else:
+            return 0
