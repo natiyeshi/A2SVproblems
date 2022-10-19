e4a2b9ef386fd91b009153e29a68403bab17247a
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-10-19 10:12:05 +0300

    Create XOR.py

---
+++ XOR.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def xorQueries(self, arr, queries):
+        res=[0]
+        for i in arr:
+            res.append(res[-1]^i)
+        final_res=[]
+        for query in queries:
+            low,high=query
+            final_res.append(res[low]^res[high+1])
+        return final_res
+            
