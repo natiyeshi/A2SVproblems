1afcfa84bb52655b6acdb78da59ae0635c0329ce
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-21 10:12:10 +0300

    Update Noidea.py

--- Noidea.py
+++ Noidea.py
@@ -1,9 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-
-nm = input()
-arr = input().split()
-A = input().split()
-B = input().split()
+n, m = input().split()
+sc_ar = input().split()
 
 
-print (sum([(i in A) - (i in B) for i in arr]))    
+A = set(input().split())
+B = set(input().split())
+
+print (sum([(i in A) - (i in B) for i in sc_ar]))
