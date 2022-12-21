4fb370a02039473f96e951944472520647a37143
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-21 18:40:40 +0300

    Time: 99 ms (60.22%), Space: 13.6 MB (12.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,10 +2,16 @@ class Solution(object):
     def isPalindrome(self, x):
         x = str(x)
         y = len(x) - 1
-        for i in range(len(x)):
+        i = 0
+        # for i in range(len(x)):
+        #     if x[i] != x[y]:
+        #         return False
+        #     y -= 1
+        while i < y:
             if x[i] != x[y]:
                 return False
-            y -= 1
+            y -= 1 
+            i += 1            
         return True
         
         
