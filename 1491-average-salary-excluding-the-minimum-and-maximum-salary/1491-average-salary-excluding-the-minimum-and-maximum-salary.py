ad99ae41c8852478d4d6f7c70d1d6f80955d01fb
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-24 12:18:15 +0300

    Time: 22 ms (77.13%), Space: 13.3 MB (92.64%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -2,13 +2,8 @@ class Solution(object):
     def average(self, salary):
         salary.sort()
 
-        av = 0
-        for ind,val in enumerate(salary):
-            if ind != 0 and ind != len(salary) - 1:
-                av += val
-        
-        average =  av / (len(salary) - 2.0)
-        return average
+        return sum(salary[1:-1]) / (len(salary) - 2.0) 
+       
         """
         :type salary: List[int]
         :rtype: float
