a2a2701bcdb893643309e2136316487ef738d935
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-25 23:34:59 +0300

    Time: 2269 ms (10.08%), Space: 14.9 MB (14.97%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, arr):
+        bools = True
+        counter = 0
+        while(bools == True):
+                bools = False
+                for  i in range(len(arr)):
+                    if arr[i] == 0:
+                        arr.pop(i)
+                        counter += 1
+                        bools = True
+                        break
+        [arr.append(0) for i in range(counter)]
+        return arr
