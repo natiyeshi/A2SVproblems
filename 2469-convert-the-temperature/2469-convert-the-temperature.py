2cb00e322e3e1acf1da3f8d1536920dabe8dbb52
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-26 18:00:33 +0300

    Time: 14 ms (92.08%), Space: 13.5 MB (38.05%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        return [celsius + 273.15,(celsius * 1.80) + 32]
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        
