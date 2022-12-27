186c634b01221def7aeac4635b6281604878e9c7
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-27 19:24:09 +0300

    Create captain.py

---
+++ captain.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num = int(input())
+arr = input()
+
+arr = arr.split(" ")
+files = {}
+
+for ar in arr:
+    if ar not in files:
+        files[ar] = 1
+    else:
+        files[ar] += 1
+
+for i in files:
+    if files[i] == 1:
+        print(i)
+        break
+
+        
