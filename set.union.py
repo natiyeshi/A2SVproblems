881863506c9b04d78fe973a9726c4bb28944b808
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-27 15:19:06 +0300

    Create set.union.py

---
+++ set.union.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+la = input().split(" ")
+b = int(input())
+lb = input().split(" ") 
+la = set(map(int,la))
+lb = set(map(int,lb))
+
+aa = la.union(lb)
+print(len(aa))
