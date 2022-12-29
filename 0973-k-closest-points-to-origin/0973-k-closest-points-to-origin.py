27a32963c063438b28d216f87d2556972184ef60
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-29 20:02:25 +0300

    Time: 614 ms (96.43%), Space: 19.4 MB (86.44%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def kClosest(self, points, k):
-    
-        ans = []
-        min_ = []
-        
         points = sorted(points, key = lambda val: val[0] **2 +val[1]**2)
         return points[:k]
           
