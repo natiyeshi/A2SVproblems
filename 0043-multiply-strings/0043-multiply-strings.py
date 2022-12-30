8efa03deb7e03987a0ecb32b6da4701d6dd99b66
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2022-12-30 17:12:20 +0300

    Time: 11 ms (99.07%), Space: 13.5 MB (81.14%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+       
+        return str(eval(num1 +'*'+ num2))
+                    
+                
+                
+                
+                 
+        
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
