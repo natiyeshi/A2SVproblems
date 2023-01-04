bf3a91d5e62a151ac605b30f400f9459c8e7530a
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-01-04 18:53:47 +0300

    Time: 646 ms (66.10%), Space: 17.5 MB (98.31%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,18 +1,17 @@
 class Solution(object):
     def equalPairs(self, grid):
-        ar = []
-        c = 0
-        for k in range(len(grid[0])):
-            st = ""
-            for i in range(len(grid)):
-                st += "," + str(grid[i][k])
-            ar.append(st[1:])
-        for i in grid:
-            st = ",".join(map(str,i))
-            for i in ar:
-                if i == st:
-                    c += 1
-        return c
+        column = []
+        counter = 0
+        for col in range(len(grid[0])):
+            temp = []
+            for row in range(len(grid)):
+                temp.append(grid[row][col])
+            column.append(temp)
+        for row in grid:
+            for col in column:
+                if row == col:
+                    counter += 1
+        return counter
     
             
     
