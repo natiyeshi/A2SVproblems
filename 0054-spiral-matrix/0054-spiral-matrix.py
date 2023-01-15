3cf582afba7b78c1d51e305f4b7e8ba21c11033e
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-01-15 20:06:04 +0300

    Time: 26 ms (42.29%), Space: 13.3 MB (70.91%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        output = []
+        directions = cycle([(0, 1), (1, 0), (0, -1), (-1, 0)])
+        direc = next(directions)
+        step = 0
+        covered = set()
+        pos = [0, -1]
+        while step < m*n:
+            pos[0] += direc[0]
+            pos[1] += direc[1]
+            output.append(matrix[pos[0]][pos[1]])
+            covered.add(tuple(pos))
+            step += 1
+
+            if not(0 <= pos[0]+direc[0] < m) or not(0 <= pos[1]+direc[1] < n) or tuple([pos[0]+direc[0], pos[1]+direc[1]]) in covered:
+                direc = next(directions)
+
+        return output 
+                    
+        
+        
