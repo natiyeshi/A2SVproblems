30a0eff43977208d5546f78abedd910cf03c069c
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-01-31 17:47:46 +0300

    Time: 23 ms (51.55%), Space: 13.7 MB (64.15%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -6,7 +6,6 @@
 class Solution(object):
     def reverseBetween(self, head, left, right):
         temp = head
-    
         if temp.next == None or left == right:
             return head
         count = 1
@@ -15,16 +14,14 @@ class Solution(object):
             count += 1
         arr = []
         start = temp
-        tp = count
-        while tp != right:
+        while count != right:
             arr.append(temp.val)
             temp = temp.next
-            tp += 1
-            
+            count += 1
         arr.append(temp.val)
-        arr.reverse()
-        for i in arr:
-            start.val = i
+        
+        for i in range(len(arr)-1,-1,-1):
+            start.val = arr[i]
             start = start.next
         return head
         
