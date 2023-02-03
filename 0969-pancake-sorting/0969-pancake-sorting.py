d0eb213b8f75f2419ade5c3c583e0c1dd7dbac40
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-02-03 14:20:30 +0300

    Time: 23 ms (90.63%), Space: 13.4 MB (48.44%) - LeetHub

---
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def pancakeSort(self, A):
+        end=len(A)
+        res=[]
+        while end>1:
+            maxInd=A.index(end) 
+            if maxInd==end-1:
+                end-=1
+                continue
+
+            if maxInd!=0:
+                A[:maxInd+1]=reversed(A[:maxInd+1])
+                res.append(maxInd+1)
+                
+            A[:end]=reversed(A[:end])
+            res.append(end)
+            
+            end-=1 #decrement end
+        return res    
