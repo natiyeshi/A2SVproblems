be793737d3f1244b871143016e6ddb71b3cdc94d
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-02-21 23:29:29 +0300

    Time: 426 ms (25.92%), Space: 22.5 MB (16.67%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def nextLargerNodes(self, head):
+        
+        res, stack = [], []
+        while head:
+            while stack and stack[-1][1] < head.val:
+                res[stack.pop()[0]] = head.val
+            stack.append([len(res), head.val])
+            res.append(0)
+            head = head.next
+        return res
+                
+            
+        
+        """
+        :type head: ListNode
+        :rtype: List[int]
+        """
+        
