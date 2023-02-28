8f6fd7014b03cdc7d8443da17501336e83c2b00d
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-02-28 16:21:35 +0300

    Time: 35 ms (85.55%), Space: 13.9 MB (23.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,39 +1,34 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-        dummy = ListNode(0)
-        temp = dummy
-        while list1 and list2:
+class Solution:
+    def mergeTwoLists(self, list1, list2, arr = None, dummy = None ) -> Optional[ListNode]:
+        if arr is None:
+            arr = ListNode()
+            dummy = arr
+        if list1 and list2:
             if list1.val > list2.val:
-                temp.next = ListNode(list2.val)
-                list2 = list2.next
-            else:
-                temp.next = ListNode(list1.val)
+                temp = ListNode(list2.val)
+                arr.next = temp
+                arr = arr.next
+                return self.mergeTwoLists(list1,list2.next,arr,dummy)
+            temp = ListNode(list1.val)
+            arr.next = temp
+            arr = arr.next
+            return self.mergeTwoLists(list1.next,list2,arr,dummy)
+        else:
+            while list1:
+                temp = ListNode(list1.val)
+                arr.next = temp
+                arr = arr.next
                 list1 = list1.next
-            temp = temp.next
+            while list2:
+                temp = ListNode(list2.val)
+                arr.next = temp
+                arr = arr.next
+                list2 = list2.next
+            return dummy.next
         
-        while list1:
-            temp.next = ListNode(list1.val)
-            temp = temp.next
-            list1 = list1.next
-        while list2:
-            temp.next = ListNode(list2.val)
-            temp = temp.next
-            list2 = list2.next
-        
-        return dummy.next
-        
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
         
