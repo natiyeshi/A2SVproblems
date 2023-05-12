37c982029e16a9a03236338e539e403687af7e05
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-05-12 18:49:57 +0300

    added loud-and-rich.python

--- loud-and-rich.py
+++ loud-and-rich.py
@@ -4,23 +4,18 @@ class Solution:
         for f,t in richer:
             graph[t].append(f)
         small = float("inf")
-        visited = set()
         def dfs(i):
             nonlocal small
-            visited.add(i)
             if small != float("inf"):
                 small = i if quiet[i] < quiet[small] else small
             else:
                 small = i
-            for j in graph[i]:
-                if j not in visited:
-                    dfs(j)
+            for i in graph[i]:
+                dfs(i)
 
         result = []
         for i in range(len(quiet)):
             dfs(i)
-            visited = set()
             result.append(small)
             small = float("inf")
-            
         return result
