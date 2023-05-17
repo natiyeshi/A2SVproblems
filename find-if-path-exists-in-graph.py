aab12ff8e26244677b5e21576a475bfffaee166a
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-05-17 18:19:44 +0300

    added find-if-path-exists-in-graph.python

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,22 +1,20 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if n == 1:return True
-        graph = defaultdict(set)
-        for i in edges:
-            graph[i[0]].add(i[1])
-            graph[i[1]].add(i[0])
-        f = False
-        visited = set()
-        def dfs(temp):
-            nonlocal f,graph
-            visited.add(temp)
-            arr = graph[temp]
-            print(arr)
-            for i in arr:
-                if i == destination:
-                    f = True
-                    return
-                if i not in visited:
-                    dfs(i)
-        dfs(source)
-        return f
+
+        dic = {i : i for i in range(n)}
+        
+        def find(ind):
+            if dic[ind] != ind:
+                return find(dic[ind])
+            return ind
+        
+        def union(x,y):
+            xrep = find(x)
+            yrep = find(y)
+            
+            dic[xrep] = yrep
+        
+        for x,y in edges:
+            union(x,y)
+        
+        return find(source) == find(destination)
