0379032fa1b026467e8fe1dd8822bb326c81d9b6
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-05-25 16:26:39 +0300

    added minimum-difference-between-largest-and-smallest-value-in-three-moves.python

--- minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -2,4 +2,11 @@ class Solution:
     def minDifference(self, nums: List[int]) -> int:
         nums.sort()
         if len(nums) < 5: return 0
-        return max(nums[1:(len(nums) - 3)]) - nums[0]
+        one = nums[len(nums) - 4] - nums[0]
+        two = nums[-1] - nums[3] 
+        temp = one
+        if len(nums) > 5:
+            temp = min(nums[-2] - nums[2],nums[-3] - nums[1])
+        else:
+            temp = nums[-2] - nums[2]
+        return min(one,two,temp)
