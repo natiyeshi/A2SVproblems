501f138db5a5ec44ef0a3d89378c81aa4470ece6
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-09-18 11:16:20 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,14 +1,12 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        result = float("inf")
-        def backtrack(num = 2,counter = 2,copy = 1):
-            nonlocal result
-            if num >= n: 
-                if num == n: 
-                    result = min(result,counter)
-                return
-            backtrack(num + copy,counter + 1,copy)
-            backtrack(num * 2,counter + 2,num)
-        backtrack()
-        if result == float("inf"): result = 0
+        
+        result = 0
+        d = 2
+
+        while n > 1:
+            while n % d == 0:
+                n //= d
+                result += d
+            d += 1
         return result
