d2d2649a4a3ea0721e959e984e81824e08944169
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-10-04 09:19:48 +0300

    added network-delay-time.python

---
+++ network-delay-time.py
@@ -0,0 +1,29 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        def dijkstra(graph, start):
+            distances = {node: float('inf') for node in range(1,n + 1)}
+            distances[start] = 0
+            visited = set()
+            priority_queue = [(0, start)]
+            while priority_queue:
+                current_distance, current_node = heapq.heappop(priority_queue)
+                if current_node in visited:
+                    continue
+                visited.add(current_node)
+
+                for neighbor, weight in graph[current_node]:
+                    distance = current_distance + weight
+                    if distance < distances[neighbor]:
+                        distances[neighbor] = distance
+                        heapq.heappush(priority_queue, (distance, neighbor))
+            return distances
+        graph = defaultdict(list)
+        for f,t,w in times:
+            graph[f].append([t,w])
+        output = dijkstra(graph,k)
+        ans = 0
+        for i in output:
+            ans = max(ans,output[i])
+        if ans == float("inf"):
+            return -1
+        return ans
