49ec096ce76023c09c9a1ab45ee090275ed211ec
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-10-11 14:14:27 +0300

    added validate-binary-search-tree.python

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -6,16 +6,19 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        arr = float("-inf")
-        b = True
-        def traverse(root):
-            nonlocal arr , b
-            if root:
-                traverse(root.left)
-                if root.val > arr:
-                    arr = root.val
-                else:
-                    b = False       
-                traverse(root.right)
-        traverse(root)
-        return b
+        print("inkkk")
+        def dfs(root,m,mi):
+            if not root:
+                return True
+            val = True
+            if root.left:
+                if root.val <= root.left.val or root.left.val <= mi:
+                    return False
+                val = val and dfs(root.left,root.val,mi)
+            if root.right:
+                if root.val >= root.right.val or root.right.val >= m:
+                    return False
+                val = val and dfs(root.right,m,root.val)
+            
+            return val
+        return dfs(root,float("inf"),float("-inf"))
