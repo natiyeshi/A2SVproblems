5d350476ffb03062dbac555e370777b7737e8e35
natnael yeshiwas <93379520+natiyeshi@users.noreply.github.com>
2023-10-17 12:07:07 +0300

    added powx-n.python

--- powx-n.py
+++ powx-n.py
@@ -1,13 +1,14 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
         if n == 0:
             return 1
-        if n < 1:
-            neg = True
-            n = abs(n)
-            x = 1 / x
-        if n % 2 == 0:
-            ans = self.myPow(x , n // 2)
-            return ans ** 2
-        return x * self.myPow(x,n - 1)
+        result = 1
+        temp = abs(n)
+        while temp > 0:
+            if temp & 1:
+                result *= x
+            x *= x
+            temp = temp // 2
+        if n < 0:
+            return 1 / result
+        return result
